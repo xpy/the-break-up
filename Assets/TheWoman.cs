7d6d987ce4aac755746ce2a7fbe0f913358ad8fc
xpy <pavloschris@gmail.com>
2019-10-18 14:26:18 +0200

    I added the woman

---
+++ Assets/TheWoman.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheWoman : MonoBehaviour
+{
+    Animator anim;
+    public float status;
+    private int realStatus;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        realStatus = Mathf.FloorToInt(status * 4f);
+        anim.SetInteger("angryness", realStatus);
+    }
+}
